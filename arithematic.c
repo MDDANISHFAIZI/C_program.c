5c42a831e5fd99221ba8b0357481d6a901dbfb87
MDDANISHFAIZI <danish88095068702@gmail.com>
2024-08-24 12:47:07 +0530

    Add this file

---
+++ arithematic.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a= 7 ,b=6, c=7, sum = a+b+c;
+    printf("the value a is %d \n the value of b is %d \n the of c is %d \n and the sum of a b and c is %d \n", a,b,c,sum );
+    printf("the remainder when %d is divided by %d will be %d",a,b,a%b);
+}
